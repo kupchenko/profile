fa0779a353f253fa9989b002b8df1ecd2528bb0c
Dmitrii Kupchenko <kupchenko.work@gmail.com>
2025-11-21 14:57:54 +0400

    fix

--- components/theme-switcher.tsx
+++ components/theme-switcher.tsx
@@ -12,7 +12,7 @@ export function ThemeSwitcher() {
       variant="ghost"
       size="sm"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      className="h-8 w-8 rounded-full px-0"
+      className="h-8 w-8 rounded-full px-0 cursor-pointer"
     >
       <Sun className="h-3.5 w-3.5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 cursor-pointer" />
       <Moon className="absolute h-3.5 w-3.5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 cursor-pointer" />
