fc78b5c3df6e93dba09d533789cec5589f1d89b2
Dmitrii Kupchenko <kupchenko.work@gmail.com>
2025-11-21 12:56:16 +0400

    fix

--- components/theme-switcher.tsx
+++ components/theme-switcher.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "./theme-provider"
-import { Button } from "@/components/ui/button"
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "./theme-provider";
+import { Button } from "@/components/ui/button";
 
 export function ThemeSwitcher() {
-  const { theme, setTheme } = useTheme()
+  const { theme, setTheme } = useTheme();
 
   return (
     <Button
@@ -14,9 +14,9 @@ export function ThemeSwitcher() {
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
       className="h-8 w-8 rounded-full px-0"
     >
-      <Sun className="h-3.5 w-3.5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-3.5 w-3.5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Sun className="h-3.5 w-3.5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 cursor-pointer" />
+      <Moon className="absolute h-3.5 w-3.5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 cursor-pointer" />
       <span className="sr-only">Toggle theme</span>
     </Button>
-  )
+  );
 }
